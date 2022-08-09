e78fc0343a201d5c803baf5795f021f1ab1e7945
victoria.buyakova <lineviktoriya@gmail.com>
2022-08-09 20:24:11 +0400

    add theme provider & normaliser

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { normalize } from 'styled-normalize';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { theme } from 'theme';
 import App from './App';
+
+const GlobalStyle = createGlobalStyle`
+  ${normalize}
+  html {
+    scroll-behavior: smooth;
+  }
+  body {
+    font-family: 'Poppins', sans-serif;
+    
+    ::-webkit-scrollbar {
+      width: 8px;
+    }
+    ::-webkit-scrollbar-thumb {
+      background-color: #78B5FA;
+      border-radius: 8px;
+    }
+  }
+`;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
---
+++ src/theme.ts
@@ -0,0 +1,6 @@
+export const theme = {
+  colors: {
+    malibu: '#78B5FA',
+
+  },
+};
