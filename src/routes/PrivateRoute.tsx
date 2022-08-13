6229ced04aa9a1b8f9841ca5c750db923b8c2f34
victoria.buyakova <lineviktoriya@gmail.com>
2022-08-14 02:53:04 +0400

    remove unused variables

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,13 +1,1 @@
-import React from 'react';
-import { PATH, ROLES } from 'routes/constants';
-
-function getDefaultPath(role: ROLES): string {
-  switch (role) {
-    case ROLES.USER: return PATH.MY_PROFILE;
-    case ROLES.PUBLIC: return PATH.SIGN_IN;
-    default:
-      throw new Error('');
-  }
-}
-
 export const PrivateRoute = ({ children }: { children: JSX.Element }) => children;
