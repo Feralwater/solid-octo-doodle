cc18d75901f84fc9fe0c25d302d57a2341e707bf
victoria.buyakova <lineviktoriya@gmail.com>
2022-08-25 23:06:03 +0400

    add docker

--- server/index.js
+++ server/index.js
@@ -1,10 +1,28 @@
 const cookieSession = require("cookie-session");
+const cookieParser = require("cookie-parser");
 const express = require("express");
 const cors = require("cors");
 const passportSetup = require("./passport");
 const passport = require("passport");
 const authRoute = require("./routes/auth");
+require('dotenv').config()
+
+
+const PORT = process.env.PORT || 5000;
 const app = express();
+
+const start = async () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server started on PORT ${PORT}`);
+        });
+    }
+    catch (e) {
+        console.log(e)
+    }
+}
+
+start();
 
 app.use(
   cookieSession({ name: "session", keys: ["privateKey"], maxAge: 24 * 60 * 60 * 100 })
@@ -22,7 +40,3 @@ app.use(
 );
 
 app.use("/auth", authRoute);
-
-app.listen("5000", () => {
-  console.log("Server is running!");
-});
