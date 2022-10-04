d3fe00b83f58b3598d196b10d6068c3c84fcb8f6
victoria.buyakova <lineviktoriya@gmail.com>
2022-10-04 19:00:05 +0200

    [BE] remove passport.js

--- server/index.js
+++ server/index.js
@@ -1,40 +1,36 @@
-const cookieParser = require("cookie-parser");
-const express = require("express");
-const cors = require("cors");
-const passportSetup = require("./service/passport");
-const passport = require("passport");
-require('dotenv').config()
-const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const mongoose = require('mongoose');
+
 const PORT = process.env.PORT || 5000;
-const router = require("./routes/index");
-const errorMiddleware = require("./middlewares/error-middleware");
+const router = require('./routes/index');
+const errorMiddleware = require('./middlewares/error-middleware');
 
 const app = express();
 
 const start = async () => {
-    try {
-        await mongoose.connect(process.env.DB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
-        app.listen(PORT, () => {
-            console.log(`Server started on PORT ${PORT}`);
-        });
-    } catch (e) {
-        console.log(e)
-    }
-}
+  try {
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    app.listen(PORT, () => {
+      console.log(`Server started on PORT ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 start();
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(cors({
-    credentials: true,
-    origin: process.env.FRONTEND_URL
+  credentials: true,
+  origin: process.env.FRONTEND_URL,
 }));
-app.use("/api", router);
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(errorMiddleware);
-
+app.use('/api', router);
+// app.use(errorMiddleware);
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
-const passport = require('passport');
 
-const CLIENT_URL = 'http://localhost:3000/';
+const { FRONTEND_URL } = process.env;
 
-router.get('/login/success', (req, res) => {
+router.get('/sign-in/success', (req, res) => {
   if (req.user) {
     res.status(200).json({
       success: true,
@@ -13,7 +12,7 @@ router.get('/login/success', (req, res) => {
   }
 });
 
-router.get('/login/failed', (req, res) => {
+router.get('/sign-in/failed', (req, res) => {
   res.status(401).json({
     success: false,
     message: 'failure',
@@ -22,17 +21,7 @@ router.get('/login/failed', (req, res) => {
 
 router.get('/logout', (req, res) => {
   req.logout();
-  res.redirect(CLIENT_URL);
+  res.redirect(FRONTEND_URL);
 });
-
-router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
-
-router.get(
-  '/google/callback',
-  passport.authenticate('google', {
-    successRedirect: CLIENT_URL,
-    failureRedirect: '/login/failed',
-  }),
-);
 
 module.exports = router;
