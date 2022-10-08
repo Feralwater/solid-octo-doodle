a184c0a3336e04f0a3e78e6033770734c4d3fb50
victoria.buyakova <lineviktoriya@gmail.com>
2022-10-09 01:32:40 +0200

    [FE] add styles to activate page

--- client/src/pages/User/Activation/index.tsx
+++ client/src/pages/User/Activation/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { ReactComponent as ActivationImage } from 'assets/svg/postman.svg';
+import { Paragraph } from 'components';
+import { Layout } from 'components/Layout/Layout';
+import { ActivationImageContainer, ActivationLayout } from './styles';
 
 const Activation = () => (
-  <h1>
-    confirm account
-  </h1>
+  <Layout>
+    <ActivationLayout>
+      <ActivationImageContainer>
+        <ActivationImage />
+      </ActivationImageContainer>
+      <Paragraph>
+        We have sent you an email with a link to activate your account.
+        Please check your inbox and click on the link to activate your account.
+      </Paragraph>
+    </ActivationLayout>
+  </Layout>
 );
 
 export default Activation;
---
+++ client/src/pages/User/Activation/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const ActivationLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+  align-items: center;
+`;
+
+export const ActivationImageContainer = styled.div`
+  display: inline-flex;
+  align-content: center;
+  justify-content: center;
+  padding: 2rem 0 0 0;
+  height: 300px;
+  width: 200px;
+`;
